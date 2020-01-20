62584b9f870557a2b6a471cd019bc33c3bd9ff25
mujahid-kh <mnk1998@gmail.com>
2020-01-20 04:12:02 +0100

    - Deprecated Cart feature - Added Inventory feature

--- app/src/main/java/com/example/dietlens/ui/inventory/InventoryViewModel.java
+++ app/src/main/java/com/example/dietlens/ui/inventory/InventoryViewModel.java
@@ -1,14 +1,14 @@
-package com.example.dietlens.ui.cart;
+package com.example.dietlens.ui.inventory;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class CartViewModel extends ViewModel {
+public class InventoryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public CartViewModel() {
+    public InventoryViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is cart fragment");
     }
