2b7cb9c2a3f8d0d7404c64a1b4d7d39716eafc79
talhahussain7 <talhahussainb@gmail.com>
2020-01-22 18:45:48 +0100

    - Implemented recyclerviews to add diary items for different meal types.

--- app/src/main/java/com/example/dietlens/ui/diary/DiaryFragment.java
+++ app/src/main/java/com/example/dietlens/ui/diary/DiaryFragment.java
@@ -24,9 +24,11 @@ import java.util.List;
 
 public class DiaryFragment extends Fragment {
 
-    ImageButton addBreakfastBtn;
-    RecyclerView recyclerViewBreakfast;
-    DiaryAdapter breakfastAdapter;
+    ImageButton addBreakfastBtn,addLunchBtn,addDinnerBtn;
+    RecyclerView recyclerViewBreakfast,recyclerViewLunch,recyclerViewDinner;
+    DiaryAdapter breakfastAdapter,lunchAdapter,dinnerAdapter;
+    List<DiaryItem> lunchList = new ArrayList<>();
+    List<DiaryItem> dinnerList = new ArrayList<>();
     List<DiaryItem> breakfastList = new ArrayList<>();
     private DiaryViewModel diaryViewModel;
 
@@ -40,20 +42,55 @@ public class DiaryFragment extends Fragment {
             public void onChanged(@Nullable String s) {
             }
         });
+        
         DiaryItem diaryItem = new DiaryItem("Nutella","Ferrero, 1 serving (0.5 o.z.)","82");
+        //Hardcoding this item and adding it to the list for visualization testing only...
         breakfastList.add(diaryItem);
+        lunchList.add(diaryItem);
+        dinnerList.add(diaryItem);
+        //RecyclerViews for different meal type
         recyclerViewBreakfast = root.findViewById(R.id.recycler_view_breakfast);
+        recyclerViewLunch=root.findViewById(R.id.recycler_view_lunch);
+        recyclerViewDinner=root.findViewById(R.id.recycler_view_dinner);
+        //Adapters for every meal type
         breakfastAdapter = new DiaryAdapter(getContext(),breakfastList);
+        lunchAdapter=new DiaryAdapter(getContext(),lunchList);
+        dinnerAdapter= new DiaryAdapter(getContext(),dinnerList);
+
+        //RecyclerViews for different meal types
         recyclerViewBreakfast.setAdapter(breakfastAdapter);
-
+        recyclerViewLunch.setAdapter(lunchAdapter);
+        recyclerViewDinner.setAdapter(dinnerAdapter);
+        // initalization of add buttons
         addBreakfastBtn = root.findViewById(R.id.add_breakfast_btn);
+        addLunchBtn=root.findViewById(R.id.add_lunch_btn);
+        addDinnerBtn=root.findViewById(R.id.add_dinner_btn);
 
+        // Click listeners for add button for different meal types.
         addBreakfastBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DiaryItem diaryItem = new DiaryItem("Nutella","Ferrero, 1 serving (0.5 o.z.)","82");
                 breakfastList.add(diaryItem);
                 breakfastAdapter.notifyDataSetChanged();
+            }
+        });
+
+        addLunchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DiaryItem lunchItem = new DiaryItem("Nutella","Ferrero, 1 serving (0.5 o.z.)","82");
+                lunchList.add(lunchItem);
+                lunchAdapter.notifyDataSetChanged();
+            }
+        });
+
+        addDinnerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DiaryItem dinnerItem = new DiaryItem("Nutella","Ferrero, 1 serving (0.5 o.z.)","82");
+                dinnerList.add(dinnerItem);
+                dinnerAdapter.notifyDataSetChanged();
             }
         });
         return root;
